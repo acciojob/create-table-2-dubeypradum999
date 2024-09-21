fd809a1357644fb0ed821972edfd2b26c28f6863
Pradumn Dubey <140749011+dubeypradum999@users.noreply.github.com>
2024-09-22 02:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
-    //Write your code here
-  
+  var rn = prompt("Input number of rows");
+  var cn = prompt("Input number of columns");
+
+  if (rn && cn && !isNaN(rn) && !isNaN(cn)) {
+    var table = document.getElementById("myTable");
+    table.innerHTML = "";
+
+    for (var i = 0; i < rn; i++) {
+      var row = table.insertRow();
+      for (var j = 0; j < cn; j++) {
+        var cell = row.insertCell();
+        cell.textContent = "Row-" + i + " Column-" + j;
+      }
+    }
+  } else {
+    alert("Please enter valid numbers for rows and columns.");
+  }
 }
